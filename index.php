0df2e5ec9e9eccda5b13ba0740378aff6d512cd9
Santiago0514 <santigo.hoyos@correo.tdea.edu.co>
2020-08-31 19:28:17 -0500

    tablas

--- index.php
+++ index.php
@@ -6,6 +6,22 @@
 </head>
 <body>
 
+<table ">
+  <caption>Título de la tabla</caption>
+  <tr>
+    <th>Nombre</th>
+    <th>Cédula</th>
+  </tr>
+  <tr>
+    <td>Juan</td>
+    <td>123</td>
+  </tr>
+  <tr>
+    <td>Ana</td>
+    <td>456</td>
+  </tr>
+</table>
+
 
 </body>
 </html>
